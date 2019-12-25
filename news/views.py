bc972e1bf9c642f0cf10d78758c9aadf2834799b
Zhipeng Wang <pengw@microfocus.com>
2019-12-25 16:46:11 +0800

    init a django practice project

---
+++ news/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+    context = {'news_list': Post.objects.all()}
+    return render(request, 'news/index.html', context=context)
